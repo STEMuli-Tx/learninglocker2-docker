b476a6e12031993551260dac9cb45111b88c506d
Emmanuel Okafor <nondefyde@gmail.com>
2024-02-06 20:49:47 +0000

    added sudo

--- build-dev.sh
+++ build-dev.sh
@@ -2,6 +2,6 @@
 set -e
 
 export DOCKER_TAG=dev
-docker build -t michzimny/learninglocker2-app:$DOCKER_TAG app
-docker build -t michzimny/learninglocker2-nginx:$DOCKER_TAG nginx
+sudo docker build -t michzimny/learninglocker2-app:$DOCKER_TAG app
+sudo docker build -t michzimny/learninglocker2-nginx:$DOCKER_TAG nginx
 
